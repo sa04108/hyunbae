2087335385e8582754b85da345c24117bfb08af0
sa04108 <sa04108@naver.com>
2020-12-17 01:49:55 +0900

    달러 모드 추가
    소수점이 붙는 달러를 매수할 경우를 추가함. 하지만 여전히 파일 경로 설정 기능이 완전하지 못하다.

--- 주식투자용/주식투자용/분할매수.cpp
+++ 주식투자용/주식투자용/분할매수.cpp
@@ -5,16 +5,10 @@
 #include <conio.h>
 using namespace std;
 
-template<typename T>
-string insertComma(T num) {
-	try {
-		string typeOfNum = typeid(num).name();
-		if (!(typeOfNum == "int" || typeOfNum == "double"))
-			throw "§int 혹은 double 형의 숫자만 ','를 삽입할 수 있습니다.";
-
+string insertComma(bool isDollar, double num) {
 		string str = to_string(num);
 		int i = str.find('.');
-		str.erase(i);
+		isDollar ? str.erase(i + 3) : str.erase(i);
 
 		int third = 0;
 		for (int j = i - 1; j > 0; j--)
@@ -28,27 +22,36 @@ string insertComma(T num) {
 		}
 
 		return str;
-	}
-	catch (const char* s) {
-		cout << s;
-		return "";
-	}
 }
 
-void print(double low, double high, unsigned int num, unsigned int money) {
+void print(double low, double high, unsigned int num, unsigned int won) {
 	double div = (high - low) / ((double)num - 1);
-	double div_money = money / (double)num;
+	double div_won = won / (double)num;
 
 	for (unsigned int i = 0; i < num; i++)
 	{
-		cout << "◆" << i + 1 << "차 매수 지점 >> " << insertComma(low + i * div) << "원";
-		cout << "\t\t◆매수 금액 >> " << insertComma(div_money) << "원";
-		cout << "\t\t◆매수 수량 >> " << fixed << setprecision(3) << div_money / (low + i * div) << "개\n";
+		cout << "◆" << i + 1 << "차 매수 지점 >> " << insertComma(false, low + i * div) << "원";
+		cout << "\t\t◆매수 금액 >> " << insertComma(false, div_won) << "원";
+		cout << "\t\t◆매수 수량 >> " << fixed << setprecision(3) << div_won / (low + i * div) << "개\n";
 	}
 	cout << "*****************************************************" << endl << endl;
 }
 
-void init(ifstream& fin, double& low, double& high, unsigned int& num, unsigned int& money) {
+void print(double low, double high, unsigned int num, double dollar) {
+	double div = (high - low) / ((double)num - 1);
+	double div_dollar = dollar / (double)num;
+
+	for (unsigned int i = 0; i < num; i++)
+	{
+		cout << "◆" << i + 1 << "차 매수 지점 >> " << insertComma(true, low + i * div) << "달러";
+		cout << "\t\t◆매수 금액 >> " << insertComma(true, div_dollar) << "달러";
+		cout << "\t\t◆매수 수량 >> " << fixed << setprecision(2) << div_dollar / (low + i * div) << "개\n";
+	}
+	cout << "*****************************************************" << endl << endl;
+}
+
+template<typename T>
+void init(ifstream& fin, double& low, double& high, unsigned int& num, T& money) {
 	fin.seekg(0, ios::end);
 	int filesize = fin.tellg();
 	int size = 0;
@@ -59,6 +62,15 @@ void init(ifstream& fin, double& low, double& high, unsigned int& num, unsigned 
 
 	while (true)
 	{
+		try {
+			string typeOfNum = typeid(money).name();
+			if (!(typeOfNum == "unsigned int" || typeOfNum == "double"))
+				throw "§unsigned int 혹은 double 형의 숫자만 매개변수로 올 수 있습니다.\n";
+		}
+		catch (const char* s) {
+			cout << s;
+			return;
+		}
 		if (filesize - offset <= 0) break;
 		d.clear();
 		for (int i = offset; i < filesize; i++)
@@ -85,7 +97,7 @@ void init(ifstream& fin, double& low, double& high, unsigned int& num, unsigned 
 			switch (flag)
 			{
 			case 0:
-				money = stoi(d);
+				money = typeid(money).name() == "unsigned int" ? stoi(d) : stod(d);
 				break;
 			case 1:
 				num = stoi(d);
@@ -108,9 +120,22 @@ void init(ifstream& fin, double& low, double& high, unsigned int& num, unsigned 
 	}
 }
 
+void setFileRoute(ofstream& fout, string& file) {
+	cout << "§데이터를 저장할 파일의 경로를 재설정합니다.\n";
+	cout << "§경로를 다음의 예시와 같이 정확하게 입력하십시오. 백슬래시를 두 번씩 입력해야 합니다.\n";
+	cout << "c:\\Users\\User\\Desktop\\data.txt\n\n";
+	cin >> file;
+
+	fout.seekp(0, ios::beg);
+	fout.write(file.c_str(), file.size());
+
+	cout << "§" << file << " 의 경로로 재지정되었습니다.\n해당 경로에 해당 파일이 있어야 다음 프로그램 실행 시 파일을 불러올 수 있습니다.\n\n";
+}
+
 int main() {
-	double low, high;
-	unsigned int money, num;
+	double low, high, dollar;
+	unsigned int won, num;
+	bool isDollar = false;
 
 	string file = "c:\\buyonscale.txt";
 	bool isFileNone = false;
@@ -130,29 +155,25 @@ int main() {
 
 	if (!isFileNone)
 	{
-		init(fin, low, high, num, money);
+		init(fin, low, high, num, won);
 
-		if (!(low < 0 || high < 0 || num < 0 || money < 0)) {
+		if (!(low < 0 || high < 0 || num < 0 || won < 0)) {
 
 			cout << "### 저장된 값 목록 ###\n";
-			cout << "저가 : " << low << "\t고가 : " << high << "\t분할 단계 수 : " << num << "\t총 매수 금액 : " << fixed << money << endl;
-			print(low, high, num, money);
+			cout << "저가 : " << low << "\t고가 : " << high << "\t분할 단계 수 : " << num << "\t총 매수 금액 : " << fixed << won << endl;
+			print(low, high, num, won);
 		}
 	}
 
-	cout << "§아무 키 입력 (r키 : 경로 재설정 / esc키 : 나가기)\n\n";
+	cout << "§아무 키 입력 (c키 : 달러 모드 / r키 : 경로 재설정 / esc키 : 나가기)\n";
 
 	int ch = _getch();
 	if (ch == 'r') {
-		cout << "§데이터를 저장할 파일의 경로를 재설정합니다.\n";
-		cout << "§경로를 다음의 예시와 같이 정확하게 입력하십시오. 백슬래시를 두 번씩 입력해야 합니다.\n";
-		cout << "c:\\Users\\User\\Desktop\\data.txt\n\n";
-		cin >> file;
-
-		fout.seekp(0, ios::beg);
-		fout.write(file.c_str(), file.size());
-
-		cout << "§" << file << " 의 경로로 재지정되었습니다.\n해당 경로에 해당 파일이 있어야 다음 프로그램 실행 시 파일을 불러올 수 있습니다.\n\n";
+		setFileRoute(fout, file);
+	}
+	else if (ch == 'c') {
+		isDollar = true;
+		cout << "§달러 모드 설정됨\n\n";
 	}
 	
 	while (ch != 27)
@@ -165,10 +186,10 @@ int main() {
 			cout << "3. 총 분할 단계 수 : ";
 			cin >> num;
 			cout << "4. 총 매수 금액 : ";
-			cin >> money;
+			isDollar ? cin >> dollar : cin >> won;
 			cout << endl;
 
-			if (low <= 0 || high <= 0 || num <= 0 || money <= 0)
+			if (low <= 0 || high <= 0 || num <= 0 || won <= 0 || dollar <= 0)
 				throw "모든 인자는 0보다 큰 값을 입력해야 합니다.";
 			if (high - low <= 0)
 				throw "고가와 저가의 입력이 잘못되었습니다.";
@@ -183,8 +204,8 @@ int main() {
 		}
 
 		if (!isFileNone)
-			fout << endl << low << endl << high << endl << num << endl << fixed << money << endl;
-		print(low, high, num, money);
+			fout << endl << low << endl << high << endl << num << endl << fixed << setprecision(3) << (isDollar ? dollar : won) << endl;
+		print(low, high, num, (isDollar ? dollar : won));
 
 		cout << "§종료를 원하시면 esc를 누르십시오. 계속 하려면 나머지 키를 누르십시오.\n";
 		ch = _getch();
